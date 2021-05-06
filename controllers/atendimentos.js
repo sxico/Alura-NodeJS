6cf692bf303e98652d86cceca6b78d04fcd1857e
Francisco G <gsfdevpool@protonmail.com>
2021-05-06 19:46:23 -0300

    Adicionado Models e criado classe e métodos para inserir atendimentos no banco de dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,12 @@
 const bodyParser = require("body-parser")
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos e utilizando GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
         res.send('Está em atendimentos realizando um POST')
     })
     
