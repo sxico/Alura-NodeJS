f90be44bdbc28b8f369206af15799551a7640afe
Francisco Gomes <sfrancisco@pop-os.localdomain>
2021-05-06 19:13:35 -0300

    Criação do arquivo de atendimentos e em seus metodos a criação de uma tabela

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const customExpress = require('./config/customExpress')
-
 const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
+
+
 conexao.connect(erro => {
     if(erro){
         console.log(erro)
     }else{
         console.log('Conectado com sucesso')
+
+        Tabelas.init(conexao)
+   
+        const app = customExpress()
+
+        app.listen(3000, () => console.info('Servidor escutando a porta 3000'))
     }
 })
 
 
-const app = customExpress()
-
-app.listen(3000, () => console.info('Servidor escutando a porta 3000'))
