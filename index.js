8ee38d4bcd86c0803758d1f83a040e20785f082f
Francisco Gomes <sfrancisco@pop-os.localdomain>
2021-05-06 18:54:55 -0300

    Criação e configuração de conexão com o Banco de Dados MySQL

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const customExpress = require('./config/customExpress')
 
+const conexao = require('./infraestrutura/conexao')
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('Conectado com sucesso')
+    }
+})
+
+
 const app = customExpress()
 
 app.listen(3000, () => console.info('Servidor escutando a porta 3000'))
