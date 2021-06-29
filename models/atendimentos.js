2aa892ed256f97e1fb395cfe8c9d3c2e3715d73c
Francisco G <gsfdevpool@protonmail.com>
2021-06-29 19:43:26 -0300

    Adicionado validação de dados de entrada e resposta para os mesmos de acordo com sua validação

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,16 +5,37 @@ class Atendimento  {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
-        
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+                
+        const validacao =[
+            {
+                nome: 'cliente',
+                valido: atendimento.cliente.length >= 5,
+                mensagem: 'Nome deve ter no mínimo 5 caracteres'
+            },
+            {
+                nome: 'data',
+                valido: moment(data).isSameOrAfter(dataCriacao),
+                mensagem: 'Data deve ser maior ou igual a data atual'
             }
-        })
+        ]
+        const erros = validacao.filter(campo => !campo.valido)
+
+        if(erros.length) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+            
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })        
+        }
+
+
     }
 }
 
