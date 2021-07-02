1e0c4fd72cfcd504ed483710ebc68b20c14039b3
Francisco G <gsfdevpool@protonmail.com>
2021-07-02 00:22:42 -0300

    Adicionado comentários

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql')
 
+/*
+ * Configuração de conexão
+*/
 const conexao = mysql.createConnection({
     host: '127.0.0.1',
     port: 3306,
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,8 +1,14 @@
 class Tabelas {
+    /*
+     * Inicia a conexão
+    */
     init(conexao)  {
         this.conexao = conexao
         this.criarAtendimentos()
     }
+    /*
+     *Criar Tabela para Atendimentos e caso exista retorna true
+    */
     criarAtendimentos() {
         const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
         this.conexao.query(sql, erro => {
