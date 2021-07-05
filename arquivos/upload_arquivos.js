e1a2ad9dce74737fa9ff7d282413ef087758e779
Francisco G <gsfdevpool@protonmail.com>
2021-07-05 20:00:05 -0300

    Adicionado verificação de: extensão do arquivo, callback reposta para: criação e erro

--- arquivos/upload_arquivos.js
+++ arquivos/upload_arquivos.js
@@ -1,5 +1,15 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.createReadStream('./assets/nest.png')
-    .pipe(fs.createWriteStream('./assets/nest_copia.png'))
-    .on('finish', () => console.log('Imagem copiada com sucesso!'))
+module.exports = (caminho, nomeArquivo, callbackImagemCriada) => {
+    const caminhoDestino = `./assets/imagens/${nomeArquivo}${path.extname(caminho)}`
+    if(['png', 'jpg', 'jpeg'].indexOf(path.extname(caminho).substring(1)) !== -1){
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(caminhoDestino))
+            .on('finish', () => callbackImagemCriada(false, caminhoDestino))
+    }else {
+        console.log('Tipo de extensão inválido!')
+        const erro = "Extensão do arquivo é inválido"
+        callbackImagemCriada(erro)
+    }
+}
--- models/pets.js
+++ models/pets.js
@@ -1,14 +1,23 @@
 const conexao = require('../infraestrutura/conexao')
-
+const uploadArquivo = require('../arquivos/upload_arquivos')
 class Pet {
     adiciona(pet, res) {
         const query = 'INSERT INTO Pets SET ?'
-        conexao.query(query, pet, erro => {
+
+        uploadArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
             if(erro){
                 res.status(400).json(erro)
             }else{
-                res.status(200).json(pet)
+                //const novoPet = {nome: pet.nome, imagem: novoCaminho}
+                conexao.query(query, {nome: pet.nome, imagem: novoCaminho}, erro => {
+                    if(erro){
+                        res.status(400).json(erro)
+                    }else{
+                        res.status(200).json({nome: pet.nome, imagem: novoCaminho})
+                    }
+                })
             }
+            
         })
     }
 }
